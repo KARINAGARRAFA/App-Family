b161cc19cdf8b89be1fc617074ac9ee720393085
karina123 <gh3karina@gmail.com>
2017-03-09 19:21:38 -0500

    Cuarto  commit

--- app/src/main/java/pe/edu/lsc/familialaureana/MainActivity.java
+++ app/src/main/java/pe/edu/lsc/familialaureana/MainActivity.java
@@ -1,13 +1,21 @@
 package pe.edu.lsc.familialaureana;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button boton;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button boton,btnInisiarSesion;
+    ImageView btnMensaje, btnLlamada,btnUbicacion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +23,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         boton = (Button) findViewById(R.id.btnRegistrase);
         boton.setOnClickListener(this);
-            }
+        btnInisiarSesion = (Button) findViewById(R.id.btnIniciarSesion);
+        btnInisiarSesion.setOnClickListener(this);
+        btnMensaje = (ImageView) findViewById(R.id.imageMensaje);
+        btnMensaje.setOnClickListener(this);
+        btnLlamada = (ImageView) findViewById(R.id.imageLlamada);
+        btnLlamada.setOnClickListener(this);
+        btnUbicacion = (ImageView) findViewById(R.id.imageUbicacion);
+        btnUbicacion.setOnClickListener(this);
+    }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        Intent i;
+        switch (v.getId()) {
             case R.id.btnRegistrase:
-                Intent i=new Intent(this,RegistroG.class);
+                i = new Intent(this, RegistroG.class);
                 startActivity(i);
                 break;
+            case R.id.btnIniciarSesion:
+                i=new Intent(this,LoginGeneral.class);
+                startActivity(i);
+                break;
+            case R.id.imageMensaje:
+                i = new Intent(this, Mensajes.class);
+                startActivity(i);
+                break;
+            case R.id.imageLlamada:
+                i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse("tel:666-666-666"));
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(i);
+                break;
+            case R.id.imageUbicacion:
+                i=new Intent(this,Ubicacion.class);
+                startActivity(i);
+                break;
+
             default:
                 break;
         }
--- app/src/main/java/pe/edu/lsc/familialaureana/RegistroG.java
+++ app/src/main/java/pe/edu/lsc/familialaureana/RegistroG.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class RegistroG extends AppCompatActivity implements View.OnClickListener{
-    Button yacuenta;
+    Button yacuenta,btnDocente,btnEstudiante,btnPadre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +15,32 @@ public class RegistroG extends AppCompatActivity implements View.OnClickListener
         setContentView(R.layout.activity_registro_g);
         yacuenta =(Button) findViewById(R.id.btnYaCuanta);
         yacuenta.setOnClickListener(this);
+        btnDocente =(Button) findViewById(R.id.btnDocente);
+        btnDocente.setOnClickListener(this);
+        btnEstudiante =(Button) findViewById(R.id.btnEstudiante);
+        btnEstudiante.setOnClickListener(this);
+        btnPadre =(Button) findViewById(R.id.btnPadre);
+        btnPadre.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        Intent i;
         switch (v.getId()){
             case R.id.btnYaCuanta:
-                Intent i=new Intent(this,LoginGeneral.class);
+                i=new Intent(this,LoginGeneral.class);
+                startActivity(i);
+                break;
+            case R.id.btnDocente:
+                i=new Intent(this,VerificacionDni.class);
+                startActivity(i);
+                break;
+            case R.id.btnEstudiante:
+                i=new Intent(this,VerificacionDni.class);
+                startActivity(i);
+                break;
+            case R.id.btnPadre:
+                i=new Intent(this,VerificacionDni.class);
                 startActivity(i);
                 break;
             default:
