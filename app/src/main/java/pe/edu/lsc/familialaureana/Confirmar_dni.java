57618ef16b19436aae26d6416e236e9e3674ff70
leonil <chipana.leo1@gmail.com>
2017-03-16 12:38:18 -0500

    alerta de confimaciones

---
+++ app/src/main/java/pe/edu/lsc/familialaureana/Confirmar_dni.java
@@ -0,0 +1,13 @@
+package pe.edu.lsc.familialaureana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Confirmar_dni extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmar_dni);
+    }
+}
--- app/src/main/java/pe/edu/lsc/familialaureana/LoginGeneral.java
+++ app/src/main/java/pe/edu/lsc/familialaureana/LoginGeneral.java
@@ -3,13 +3,17 @@ package pe.edu.lsc.familialaureana;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginGeneral extends AppCompatActivity {
     Button ads;
+    TextView usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_general);
+        usuario = (TextView) findViewById(R.id.email);
+
     }
 }
